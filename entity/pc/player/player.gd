7b32f4c28edda47ba2ecc98be2247f9769795b17
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-15 22:57:30 -0500

    Add double jumping capabilities to player

--- entity/pc/player/player.gd
+++ entity/pc/player/player.gd
@@ -6,16 +6,20 @@ extends KinematicBody2D
 const GRAV = 20
 const JUMP_POWER = 500
 const SPEED = 500
+export var max_jumps = 2
 var motion = Vector2(0, 0)
 var UP = Vector2(0,-1)
+var consecutive_jumps = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
 	pass
 
-func checkAndExec():
-	if Input.is_action_pressed("ui_up") and is_on_floor():
+func check_and_exec():
+	if Input.is_action_just_pressed("ui_up") and is_jumpable():
 		motion.y = -JUMP_POWER
+		consecutive_jumps += 1
 	if Input.is_action_pressed("ui_left"):
 		motion.x = -SPEED
 	elif Input.is_action_pressed("ui_right"):
@@ -25,8 +29,22 @@ func checkAndExec():
 	motion.y += GRAV
 	motion = move_and_slide(motion, UP)
 
+func is_jumpable():
+	if consecutive_jumps <= max_jumps:
+		return true
+	else:
+		return false
+
+func process_state():
+	if is_on_floor():
+		consecutive_jumps = 0
+
 func _physics_process(delta):
-	checkAndExec()
+	#print(consecutive_jumps)
+	check_and_exec()
+	process_state()
+	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
