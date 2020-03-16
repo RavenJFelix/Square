c1204126b60abbc5df815329f60670adb555b7d5
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-16 03:04:51 -0500

    Add ladder logic

--- map/motion/ladder/ladder.gd
+++ map/motion/ladder/ladder.gd
@@ -11,3 +11,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_StaticBody2D_body_entered(body):
+	if body.has_method("mode_climb"):
+		body.mode_climb()
+
+
+func _on_StaticBody2D_body_exited(body):
+	if body.has_method("mode_normal"):
+		body.mode_normal()
