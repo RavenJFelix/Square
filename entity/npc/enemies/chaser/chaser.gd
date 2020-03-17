9e1a730ec6537d552e869967fe41af913e8b965f
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-17 14:13:19 -0500

    Move missile code to chaser

--- entity/npc/enemies/chaser/chaser.gd
+++ entity/npc/enemies/chaser/chaser.gd
@@ -1,13 +1,15 @@
 extends KinematicBody2D
-enum MODE {chase, search}
+var explosion = load("res://effects/simple-explosion/simple-explosion.tscn")
 var vel = Vector2()
-var start_speed = 100
+export var start_speed = 100
+export var timeout = 10
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 var target
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Timer.start(timeout)
 	vel = Vector2(0, -start_speed).rotated(rotation)
 
 func _physics_process(delta):
@@ -16,6 +18,15 @@ func _physics_process(delta):
 #func _process(delta):
 #	pass
 
+func _explode():
+	var boom = explosion.instance()
+	get_parent().add_child(boom)
+	boom.global_position = global_position
+	queue_free()
 
 func _on_Area2D_body_entered(body):
-	pass # Replace with function body.
+	_explode()
+
+
+func _on_Timer_timeout():
+	_explode()
