63d60323d92d3d1eadaff84f0e4614509fa5af3c
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-17 14:38:33 -0500

    Add object detection capabilities

--- entity/npc/enemies/chaser/chaser.gd
+++ entity/npc/enemies/chaser/chaser.gd
@@ -1,22 +1,39 @@
 extends KinematicBody2D
+enum MODE {search, chase}
+enum MOVE_MODE {x,y}
 var explosion = load("res://effects/simple-explosion/simple-explosion.tscn")
 var vel = Vector2()
 export var start_speed = 100
 export var timeout = 10
+var target_mode = MOVE_MODE.x
+var mode = MODE.search
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var target
+var target = NodePath()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Timer.start(timeout)
 	vel = Vector2(0, -start_speed).rotated(rotation)
 
 func _physics_process(delta):
+	match mode:
+		MODE.chase:
+			_chase()
+		MODE.search:
+			_search()
 	vel = move_and_slide(vel)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-#	pass
+func _chase():
+	print("CHASING")
+func _search():
+	print("SEARCHING")
+	var bodies = $TargetArea.get_overlapping_bodies()
+	for body in bodies:
+		if body.has_method("is_player"):
+			target = body
+			mode = MODE.chase
 
 func _explode():
 	var boom = explosion.instance()
