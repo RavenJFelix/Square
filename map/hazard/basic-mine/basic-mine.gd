85fc0a3226b6df20eac8fc196994f9c361cc0c20
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-16 21:52:05 -0500

    Use simple exlposions

--- map/hazard/basic-mine/basic-mine.gd
+++ map/hazard/basic-mine/basic-mine.gd
@@ -1,8 +1,9 @@
 extends Polygon2D
-var explosion = load("res://effects/explosion/explosion.tscn")
+var explosion = load("res://effects/simple-explosion/simple-explosion.tscn")
 
 func _on_StaticBody2D_body_entered(body):
 	if body.has_method("is_player"):
 		var boom = explosion.instance()
 		get_parent().add_child(boom)
+		boom.global_position = $Position2D.global_position
 		queue_free()
