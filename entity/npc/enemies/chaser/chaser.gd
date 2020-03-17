ceed9cc0c34b4a1e4644d52cddae9828126f9af9
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-17 13:44:36 -0500

    Move missile script to missile folder

--- entity/npc/enemies/chaser/chaser.gd
+++ entity/npc/enemies/chaser/chaser.gd
@@ -15,3 +15,7 @@ func _physics_process(delta):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Area2D_body_entered(body):
+	pass # Replace with function body.
