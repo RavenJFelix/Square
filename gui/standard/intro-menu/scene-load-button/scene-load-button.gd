1b1a87c6848266b5fe29d8be547859f799058e70
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-22 14:22:38 -0500

    Move scene buttons to level buttons

--- gui/standard/intro-menu/scene-load-button/scene-load-button.gd
+++ gui/standard/intro-menu/scene-load-button/scene-load-button.gd
@@ -3,7 +3,7 @@ extends Button
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-export var level = ""
+export var scene = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
